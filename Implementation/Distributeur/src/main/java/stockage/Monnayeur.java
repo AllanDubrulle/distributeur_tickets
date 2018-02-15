8fc18c303dedfc588ae24dcfc1396156bb4502f4
Allandub <allandu1@live.fr>
2018-02-15 22:50:51 +0100

    modif monnayeur

--- Implementation/Distributeur/src/main/java/stockage/Monnayeur.java
+++ Implementation/Distributeur/src/main/java/stockage/Monnayeur.java
@@ -2,47 +2,64 @@ package stockage;
 
 import java.util.HashMap;
 
-public class Monnayeur extends interfaceGraphique.ComposantVisible {
+public class Monnayeur extends interfaceGraphique.ComposantVisible
+{
 	
-	public Monnayeur(boolean enMarche) {
+	public Monnayeur(boolean enMarche)
+	{
 		super(enMarche);
 	}
 
 	private HashMap<Pieces, Integer > StockPiece; 
 	private HashMap<Billet,Integer> StockBillet;
 	private Pieces[] ordrePieces= {Pieces.P200,Pieces.P100,Pieces.P50,Pieces.P20,Pieces.P10,Pieces.P5,Pieces.P2,Pieces.P1};;  
-	private Billet[] ordreBillet= {Billet.B50,Billet.B20,Billet.B10,Billet.B5};
+	private Billet[] ordreBillets= {Billet.B50,Billet.B20,Billet.B10,Billet.B5};
 	
-	public Rendu retournerArgent(int montant) throws PasAssezDeMonnaie,ComposantHorsService {
-		if (!estEnMarche()) {
+	public Rendu retournerArgent(int montant) throws PasAssezDeMonnaie,ComposantHorsService
+	{
+		if (!estEnMarche())
+		{
 			throw new ComposantHorsService("Monnayeur hors service");
 		}
 		HashMap<Pieces,Integer> renduPiece = new HashMap<Pieces,Integer>();
 		HashMap<Billet,Integer> renduBillet = new HashMap<Billet,Integer>();
-		for (int i =0;i< ordreBillet.length ;i++) {
+		for (int i =0;i< ordreBillets.length ;i++)
+		{
 			int nbr = 0;
-			while(montant%ordreBillet[i].valeur()==0) {
-					if (StockBillet.get(ordreBillet[i])!=0) {
-						nbr++;
-						montant-=ordreBillet[i].valeur();
-					}
+			while(montant%ordreBillets[i].valeur()==0 && StockBillet.get(ordreBillets[i])!=0)
+			{
+				nbr++;
+				montant-=ordreBillets[i].valeur();
 			}
-			renduBillet.put(ordreBillet[i], nbr);
-			StockBillet.put(ordreBillet[i], StockBillet.get(ordreBillet[i])-nbr);
+			renduBillet.put(ordreBillets[i], nbr);
 		}
-		for (int i =0;i< ordrePieces.length ;i++) {
+		for (int i =0;i< ordrePieces.length ;i++)
+		{
 			int nbr = 0;
-			while(montant%ordrePieces[i].valeur()==0) {
-				if (StockPiece.get(ordrePieces[i])!=0) {
-					nbr++;
-					montant-=ordrePieces[i].valeur();
-				}
+			while(montant%ordrePieces[i].valeur()==0 && StockPiece.get(ordrePieces[i])!=0)
+			{
+				nbr++;
+				montant-=ordrePieces[i].valeur();
 			}
 			renduPiece.put(ordrePieces[i], nbr);
-			StockPiece.put(ordrePieces[i], StockPiece.get(ordrePieces[i])-nbr);
 		}
-		if (montant!=0) {
+		
+		if (montant!=0)
+		{
 			throw new PasAssezDeMonnaie("Impossible de rendre la monnaie");
+		}
+		
+		else
+		{
+			for (int i =0;i< ordreBillets.length ;i++)
+			{
+				StockBillet.put(ordreBillets[i],StockBillet.get(ordreBillets[i])- renduBillet.get(ordreBillets[i]));
+			}
+			for (int i =0;i< ordrePieces.length ;i++)
+			{
+				StockPiece.put(ordrePieces[i],StockPiece.get(ordrePieces[i])- renduPiece.get(ordrePieces[i]));
+			}
+			
 		}
 		return new Rendu(renduPiece, renduBillet);
 	}
