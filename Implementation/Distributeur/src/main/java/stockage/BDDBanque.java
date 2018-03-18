54923b7cfa4bcdc9396c4c3d3279357ab4b23a95
VictorV <victorverhoye@gmail.com>
2018-03-18 12:12:58 +0100

    ajout methodes bddbanque

--- Implementation/Distributeur/src/main/java/stockage/BDDBanque.java
+++ Implementation/Distributeur/src/main/java/stockage/BDDBanque.java
@@ -1,10 +1,75 @@
 package stockage;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 class BDDBanque extends GestionBaseDeDonnees 
 {
 	public BDDBanque()
     {
         super();
     }
-
+	
+	public boolean verifPaiement(String numero, int PIN, double montant)
+	{
+		try
+        {
+			String requete = "SELECT SOLDE FROM CARTE WHERE (NUMERO = ? and CODE = ?)";
+            PreparedStatement declar = this.connexion.prepareStatement(requete);
+            declar.setString(1, numero);
+			declar.setInt(2, PIN);
+			ResultSet res = declar.executeQuery();
+			boolean ok = res.next();
+			if (ok && res.getDouble(1) >= montant)
+			{
+        		return true;
+			}
+			else
+			{
+				return false;
+			}
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+	}
+	
+	public void actualiserSolde(String numero, double montant)
+	{
+		try
+        {
+			Double solde = soldeCarte(numero);
+			String requete = "UPDATE carte SET solde = ? WHERE numero = ?";
+            PreparedStatement declar = this.connexion.prepareStatement(requete);
+            declar.setDouble(1, solde - montant);
+			declar.setString(2, numero);
+			declar.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+	}
+	
+	public double soldeCarte(String numero)
+	{
+		try
+        {
+			String requete = "select solde from carte where numero = ?";
+            PreparedStatement declar = this.connexion.prepareStatement(requete);
+            declar.setString(1, numero);
+			ResultSet res = declar.executeQuery();
+			double solde = res.getDouble(1);
+			return solde;
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+		return -1;
+	}
+	
 }
--- Implementation/Distributeur/src/main/java/stockage/TestBDD.java
+++ Implementation/Distributeur/src/main/java/stockage/TestBDD.java
@@ -5,11 +5,13 @@ import java.sql.SQLException;
 
 public class TestBDD {
 	public static void main (String args[]) throws SQLException {
-		HoraireTrains gest = new HoraireTrains();
+		//GestionBaseDeDonnees gest = new GestionBaseDeDonnees();
+		BDDBanque gest = new BDDBanque();
 		gest.connexion();
-		ResultSet res = gest.resultatRequete("select * from horaire");
-		HoraireTrains.afficherResultat(res);
-		//System.out.print(res);
+		//ResultSet res = gest.resultatRequete("select * from carte");
+		boolean res = gest.verifPaiement("0000000000000000000000000", 1234, 500);
+		//HoraireTrains.afficherResultat(res);
+		System.out.print(res);
 		gest.deconnexion();
 	}	
 }
