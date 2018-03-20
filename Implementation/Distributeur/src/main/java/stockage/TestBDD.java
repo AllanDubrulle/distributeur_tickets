6bce12bbf078172f00ad64b49a9e92f8f733070d
VictorV <victorverhoye@gmail.com>
2018-03-20 21:29:23 +0100

    resolution conflit

--- Implementation/Distributeur/src/main/java/stockage/TestBDD.java
+++ Implementation/Distributeur/src/main/java/stockage/TestBDD.java
@@ -11,14 +11,14 @@ public class TestBDD {
 		GestionBaseDeDonnees gest = new GestionBaseDeDonnees();
 		//BDDBanque gest = new BDDBanque();
 		gest.connexion();
-		gest.mettreAJour("insert into carte values ('', '1234', '1000.00')");
-		//ResultSet res = gest.resultatRequete("select * from carte");
+		//gest.mettreAJour("update carte set solde = '500000' where code = '4567'");
+		ResultSet res = gest.resultatRequete("select * from carte");
 		//ArrayList<String> res = gest.listeDesAbonnements();
 		//String[] res = gest.infoCarte("67030615781202136");
 		//double res = gest.calculerPrixPass("10Trajets");
 		//boolean res = gest.verifPaiement("0000000000000000000000000", 1234, 500);
 		//int res = gest.numeroAbonnementSuivant();
-		//HoraireTrains.afficherResultat(res);
+		HoraireTrains.afficherResultat(res);
 		//for (int i = 0; i < res.length; i++)
 		//{
 			//System.out.println(res[i]);
