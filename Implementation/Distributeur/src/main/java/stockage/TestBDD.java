9b5948b0a287b465d33c202f2e5803b112b652cb
VictorV <victorverhoye@gmail.com>
2018-03-29 13:14:44 +0200

    bdd modifiee

--- Implementation/Distributeur/src/main/java/stockage/TestBDD.java
+++ Implementation/Distributeur/src/main/java/stockage/TestBDD.java
@@ -7,16 +7,16 @@ public class TestBDD {
 	public static void main (String args[]) throws SQLException {
 		//BDDTitre gest = new BDDTitre();
 		//HoraireTrains gest = new HoraireTrains();
-		GestionBaseDeDonnees gest = new GestionBaseDeDonnees();
+		BDDBanque gest = new BDDBanque();
 		//BDDBanque gest = new BDDBanque();
 		gest.connexion();
-		//gest.actualiserSolde("67030615781202136", 1000000);
+		//gest.actualiserSolde("54320439576808884", 1000000);
 		//System.out.println(gest.soldeCarte("67030615781202136"));
 		ResultSet res = gest.resultatRequete("select * from abosexistants");
 		//ArrayList<String> res = gest.listeDesAbonnements();
 		//String[] res1 = gest.conversionRequeteEnTableau(res);
 		//double res = gest.calculerPrixPass("10Trajets");
-		//gest.mettreAJour("UPDATE abosexistants SET annee = 2018, mois = 12, jour = 3 WHERE numeroabo = '2'");
+		//gest.mettreAJour("update abosexistants set reduction = 'Parent employe', annee = 2018, mois = 12, jour = 3 where numeroabo = 2");
 		//boolean res = gest.verifPaiement("0000000000000000000000000", 1234, 500);
 		//int res = gest.numeroAbonnementSuivant();
 		HoraireTrains.afficherResultat(res);
